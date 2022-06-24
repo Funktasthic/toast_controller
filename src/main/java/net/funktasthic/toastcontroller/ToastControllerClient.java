f51972208112c3c1d0b8d1d71e4d24a3bc9707f6
Funktasthic <xfunktastic.ff@gmail.com>
2022-06-24 15:04:18 -0400

    Initial commit

---
+++ src/main/java/net/funktasthic/toastcontroller/ToastControllerClient.java
@@ -0,0 +1,11 @@
+package net.funktasthic.toastcontroller;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ToastControllerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
